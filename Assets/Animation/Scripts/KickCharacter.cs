fff2c6a05d2f416a02b3c0d99753a872b891018b
Erik <erikroystan@gmail.com>
2019-07-23 16:46:13 -0700

    Initial commit.

---
+++ Assets/Animation/Scripts/KickCharacter.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System;
+using System.Collections;
+
+public class KickCharacter : MonoBehaviour
+{
+    [SerializeField]
+    float acceleration = 10;
+
+    [SerializeField]
+    float maxSpeed = 7;
+
+    [SerializeField]
+    float minSpeed = 2;
+
+    [SerializeField]
+    float speedChangeInterval = 0.5f;
+
+    [SerializeField]
+    float punchDuration = 0.5f;
+
+    private float currentTargetSpeed;
+    private bool punching;
+
+    public float speed { get; private set; }
+
+    public event Action OnKick;
+
+    private void Awake()
+    {
+        currentTargetSpeed = minSpeed;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            currentTargetSpeed = Mathf.Clamp(currentTargetSpeed + speedChangeInterval, minSpeed, maxSpeed);
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            currentTargetSpeed = Mathf.Clamp(currentTargetSpeed - speedChangeInterval, minSpeed, maxSpeed);
+
+        float moveInput = Input.GetAxisRaw("Horizontal");
+
+        if (!punching)
+            speed = Mathf.MoveTowards(speed, currentTargetSpeed * moveInput, acceleration * Time.deltaTime);
+
+        if (!punching && Input.GetKeyDown(KeyCode.Space))
+        {
+            speed = 0;
+            StartCoroutine(Punch());
+        }
+
+        transform.position += Vector3.right * speed * Time.deltaTime;
+    }
+
+    private IEnumerator Punch()
+    {
+        OnKick?.Invoke();
+
+        punching = true;
+
+        yield return new WaitForSeconds(punchDuration);
+
+        punching = false;
+    }
+}
---
+++ Assets/MonoBehaviourCallbacks/MonoBehaviourCallbacks.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class MonoBehaviourCallbacks : MonoBehaviour 
+{
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        
+    }
+}
