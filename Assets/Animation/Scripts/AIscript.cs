4c613d8ef7c42c81cc1c5e2ff5b96637b256c4a9
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-11-11 16:53:08 -0800

    Assignment 3

---
+++ Assets/Animation/Scripts/AIscript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIscript : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Transform enemyTransform;
+    public Animator anim;
+    private bool wavingBack;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        StartCoroutine(WaveBack());
+    }
+
+    private IEnumerator WaveBack()
+    {
+        wavingBack = true;
+        if (Vector3.Distance(playerTransform.position, enemyTransform.position) < 100f)
+        {
+            anim.SetTrigger("WaveBack");
+        }
+        yield return new WaitForSeconds(3);
+        wavingBack = false;
+    }
+}
