271aa6075cbdda819e38e3fe985db42426a91daf
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-11-11 16:59:34 -0800

    Assignment 3

--- Assets/Animation/Scripts/KickCharacter.cs
+++ Assets/Animation/Scripts/KickCharacter.cs
@@ -1,66 +1,71 @@
 using UnityEngine;
-using System;
 using System.Collections;
 
 public class KickCharacter : MonoBehaviour
 {
     [SerializeField]
     float acceleration = 10;
-
     [SerializeField]
     float maxSpeed = 7;
-
     [SerializeField]
     float minSpeed = 2;
-
     [SerializeField]
     float speedChangeInterval = 0.5f;
-
     [SerializeField]
-    float punchDuration = 0.5f;
+    float waveDuration = 3.0f;
 
     private float currentTargetSpeed;
-    private bool punching;
+    private bool wave;
+    public GameObject friend;
+    public Animator anim;
+    private float speed;
+    public ParticleSystem friendSparkles;
 
-    public float speed { get; private set; }
-
-    public event Action OnKick;
 
     private void Awake()
     {
+        anim = GetComponent<Animator>();
         currentTargetSpeed = minSpeed;
     }
 
     private void Update()
     {
+        #region Logic
         if (Input.GetKeyDown(KeyCode.UpArrow))
             currentTargetSpeed = Mathf.Clamp(currentTargetSpeed + speedChangeInterval, minSpeed, maxSpeed);
 
         if (Input.GetKeyDown(KeyCode.DownArrow))
             currentTargetSpeed = Mathf.Clamp(currentTargetSpeed - speedChangeInterval, minSpeed, maxSpeed);
 
-        float moveInput = Input.GetAxisRaw("Horizontal");
+        float moveInput = Input.GetAxisRaw("Vertical");
+        float rotInput = Input.GetAxisRaw("Horizontal");
 
-        if (!punching)
+        if (!wave)
             speed = Mathf.MoveTowards(speed, currentTargetSpeed * moveInput, acceleration * Time.deltaTime);
 
-        if (!punching && Input.GetKeyDown(KeyCode.Space))
+        if (!wave && Input.GetMouseButtonDown(0))
         {
             speed = 0;
-            StartCoroutine(Punch());
+            StartCoroutine(Wave());
         }
 
         transform.position += Vector3.right * speed * Time.deltaTime;
+
+        #endregion
+        anim.SetFloat("Speed", speed);
     }
 
-    private IEnumerator Punch()
+    private IEnumerator Wave()
     {
-        OnKick?.Invoke();
-
-        punching = true;
-
-        yield return new WaitForSeconds(punchDuration);
-
-        punching = false;
+        wave = true;
+        friendSparkles.GetComponent<ParticleSystem>().Play();
+        GetComponent<Animator>().SetTrigger("Wave");
+        if(Vector3.Distance(GetComponent<Transform>().position, friend.GetComponent<Transform>().position) < 100f)
+        {
+            friendSparkles.GetComponent<ParticleSystem>().Play();
+            friend.GetComponent<Animator>().SetTrigger("WaveBack");
+        }
+        yield return new WaitForSeconds(waveDuration);
+        wave = false;
     }
 }
